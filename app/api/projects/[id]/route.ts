301f6778f1f2f0a84b284d85f7b2b4892e5a62b4
EnzoMarion <marionenzo26@gmail.com>
2025-02-20 20:03:19 +0100

    up

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 // Supprimer un projet (DELETE)
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
-    const { id } = context.params;
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params;
 
     try {
         const project = await prisma.project.delete({
@@ -17,8 +17,8 @@ export async function DELETE(req: NextRequest, context: { params: { id: string }
 }
 
 // Mise à jour d'un projet (PUT)
-export async function PUT(req: NextRequest, context: { params: { id: string } }) {
-    const { id } = context.params;
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params;
     const { title, description, imageUrl, moreUrl } = await req.json();
 
     try {
@@ -31,5 +31,23 @@ export async function PUT(req: NextRequest, context: { params: { id: string } })
     } catch (error) {
         console.error(error);
         return NextResponse.json({ error: "Erreur lors de la mise à jour du projet" }, { status: 500 });
+    }
+}
+
+// Récupérer un projet (GET)
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params;
+
+    try {
+        const project = await prisma.project.findUnique({
+            where: { id },
+        });
+        if (!project) {
+            return NextResponse.json({ error: "Projet non trouvé" }, { status: 404 });
+        }
+        return NextResponse.json(project, { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: "Erreur lors de la récupération du projet" }, { status: 500 });
     }
 }
