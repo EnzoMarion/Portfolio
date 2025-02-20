3cbe7523fca1744103c48c4b4b7ccd2996eb9ed1
EnzoMarion <marionenzo26@gmail.com>
2025-02-20 21:39:28 +0100

    last correction

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,14 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-interface Params {
-    params: { id: string };
-}
-
-export async function GET(request: NextRequest, { params }: Params) {
+// Récupérer un projet par ID (GET)
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { id: string } } // Assurer que params.id est une string
+) {
     try {
         const project = await prisma.project.findUnique({
-            where: { id: params.id },
+            where: { id: params.id }, // L'ID est bien une string
         });
 
         if (!project) {
@@ -18,14 +18,15 @@ export async function GET(request: NextRequest, { params }: Params) {
         return NextResponse.json(project, { status: 200 });
     } catch (error) {
         console.error(error);
-        return NextResponse.json(
-            { error: "Erreur lors de la récupération du projet" },
-            { status: 500 }
-        );
+        return NextResponse.json({ error: "Erreur lors de la récupération du projet" }, { status: 500 });
     }
 }
 
-export async function PUT(request: NextRequest, { params }: Params) {
+// Mettre à jour un projet (PUT)
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {
     try {
         const { title, description, imageUrl, moreUrl } = await request.json();
 
@@ -37,14 +38,15 @@ export async function PUT(request: NextRequest, { params }: Params) {
         return NextResponse.json(updatedProject, { status: 200 });
     } catch (error) {
         console.error(error);
-        return NextResponse.json(
-            { error: "Erreur lors de la mise à jour du projet" },
-            { status: 500 }
-        );
+        return NextResponse.json({ error: "Erreur lors de la mise à jour du projet" }, { status: 500 });
     }
 }
 
-export async function DELETE(request: NextRequest, { params }: Params) {
+// Supprimer un projet (DELETE)
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {
     try {
         const deletedProject = await prisma.project.delete({
             where: { id: params.id },
@@ -53,9 +55,6 @@ export async function DELETE(request: NextRequest, { params }: Params) {
         return NextResponse.json(deletedProject, { status: 200 });
     } catch (error) {
         console.error(error);
-        return NextResponse.json(
-            { error: "Erreur lors de la suppression du projet" },
-            { status: 500 }
-        );
+        return NextResponse.json({ error: "Erreur lors de la suppression du projet" }, { status: 500 });
     }
 }
