1b4e6371ce2c57af60dec5670facd65c6d90eab1
EnzoMarion <marionenzo26@gmail.com>
2025-02-21 12:32:10 +0100

    Ajout réaction et commentaire

--- app/api/projects/[id]/reactions/route.ts
+++ app/api/projects/[id]/reactions/route.ts
@@ -43,7 +43,7 @@ export async function POST(request: NextRequest, context: any) {
 }
 
 // Supprimer une réaction
-export async function DELETE(request: NextRequest, _context: any) {
+export async function DELETE(request: NextRequest) {
     const { userId, projectId } = await request.json();
     try {
         // Vérifier si la réaction existe pour ce projet
