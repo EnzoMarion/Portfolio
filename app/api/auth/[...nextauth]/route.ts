1a1b559c474c9bf82a1a582c65ed7a77f548f33f
EnzoMarion <marionenzo26@gmail.com>
2025-03-11 19:31:42 +0100

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-export const { handlers } = NextAuth(authOptions);
-
-export const GET = handlers.GET;
-export const POST = handlers.POST;
+// Exporte directement les handlers GET et POST
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions);
